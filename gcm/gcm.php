f77f91716eda6a0431f46407cec2ff7839d3f43d
ranjeshgupta <ranjesh.gupta@gmail.com>
2016-06-03 00:29:21 +0530

    firebox nighty added

--- gcm/gcm.php
+++ gcm/gcm.php
@@ -14,22 +14,119 @@
 		}
 		echo $reg_id;
 		
+		if(isset($_POST["browser"])){
+			$browser = $_POST["browser"];
+		}
+		else{
+			$browser = "chrome";
+		}
+		
+		$arr_reg_id = array($reg_id, $browser);
+		$arr_reg_id = json_encode($arr_reg_id);		
+		
 		//use database instead of text file
 		//and add regId to table if not exits
 		$gcmRegIDs_content  = file_get_contents("GcmRegIds.txt");
 		$gcmRegIds = explode("\n", $gcmRegIDs_content);
 		$is_reg_id_exists = false;
 		foreach($gcmRegIds as $regId){
-			if($regId == $reg_id){
+			if($regId == $arr_reg_id){
 				$is_reg_id_exists = true;
 				break;
 			}
 		}
 		
 		if($is_reg_id_exists===false)
-			file_put_contents("GcmRegIds.txt", "\n".$reg_id, FILE_APPEND | LOCK_EX);
+			file_put_contents("GcmRegIds.txt", "\n".$arr_reg_id, FILE_APPEND | LOCK_EX);
 	}
 	
+	if(isset($_GET["push"])) {
+		$gcmRegIDs_content  = file_get_contents("GcmRegIds.txt");
+		$gcmRegIds = explode("\n", $gcmRegIDs_content);
+		echo"<pre>";
+		print_r($gcmRegIds);
+		//echo "</pre>";
+		
+		$message = array(
+							"title"=>"My gcm title",
+							"body"=>"hi there msg from gcm",
+							"icon"=>"/gcm/images/push-icon.png",
+							"url"=>"/info.php"
+						);
+		put_latest_gcm_json($message);
+		$pushStatus = send_push_message($gcmRegIds, $message);
+		echo $pushStatus ."<br>";
+	}
+	
+	function send_push_message($subscriptionIDs, $message){
+		if (empty($subscriptionIDs)) return FALSE;
+		$chs = $sChrome = array();
+		$mh = curl_multi_init();
+		$aCurlHandles = array();
+		foreach ($subscriptionIDs as $subscription){
+			$subscription = json_decode($subscription);
+			print_r($subscription);
+			echo "<br>";
+			$i = count($chs);
+			switch ($subscription[1]){
+				case "firefox":
+					echo "firefox<br>";
+					$chs[ $i ] = curl_init();
+					curl_setopt($chs[ $i ], CURLOPT_URL, "https://updates.push.services.mozilla.com/push/v1/".$subscription[0] );
+					curl_setopt($chs[ $i ], CURLOPT_PUT, TRUE);
+					curl_setopt($chs[ $i ], CURLOPT_HTTPHEADER, array( "TTL: 86400" ) );
+					curl_setopt($chs[ $i ], CURLOPT_RETURNTRANSFER, TRUE);
+					curl_setopt($chs[ $i ], CURLOPT_SSL_VERIFYPEER, FALSE);
+
+					$aCurlHandles[$i] = $chs[ $i ];
+					curl_multi_add_handle($mh, $chs[ $i ]);
+				break;
+				case "chrome":
+					echo "chrome<br>";
+					$sChrome[] = $subscription[0];
+				break;    
+			}
+		}
+		if (!empty($sChrome)){
+			$fields = array(
+				'registration_ids' => $sChrome,
+				'collapse_key' => 'my_collapsed_key',
+				'data' => $message,
+			);
+			$i = count($chs);
+			$chs[ $i ] = curl_init();
+			curl_setopt($chs[ $i ], CURLOPT_URL, "https://android.googleapis.com/gcm/send" );
+			curl_setopt($chs[ $i ], CURLOPT_POST, TRUE);
+			curl_setopt($chs[ $i ], CURLOPT_HTTPHEADER, array( "Authorization: key=AIzaSyD1fz_RHNGniz_LSr__o9QMBm3qiMdmxTQ", "Content-Type: application/json" ) );
+			curl_setopt($chs[ $i ], CURLOPT_RETURNTRANSFER, TRUE);
+			curl_setopt($chs[ $i ], CURLOPT_SSL_VERIFYPEER, FALSE);			
+			curl_setopt($chs[ $i ], CURLOPT_POSTFIELDS, json_encode($fields) );
+			
+			$aCurlHandles[$i] = $chs[ $i ];
+			curl_multi_add_handle($mh, $chs[ $i ]);
+		}
+
+		do {
+			curl_multi_exec($mh, $running);
+			curl_multi_select($mh);
+		} while ($running > 0);
+
+		for ($i = 0; $i < count($chs); $i++){
+			$html = curl_multi_getcontent($chs[ $i ]); 
+			echo $html."<br>";
+			
+			curl_multi_remove_handle($mh, $chs[ $i ]);
+		} 
+
+		curl_multi_close($mh);
+	}
+	
+	function put_latest_gcm_json($message){
+		file_put_contents("GcmJson.txt", json_encode($message));
+		echo "Writing json to text file done.<br>";
+	}
+	
+	/*
 	//Post message to GCM when submitted	
 	if(isset($_GET["push"])) {
 		//db will be used to contains registatoin_ids so pick data from table
@@ -39,6 +136,7 @@
 		echo"<pre>";
 		print_r($gcmRegIds);
 		echo "</pre>";
+		die();
 		$message = array(
 							"title"=>"My gcm title",
 							"body"=>"hi there msg from gcm",
@@ -49,18 +147,15 @@
 		$pushStatus = sendMessageThroughGCM($gcmRegIds, $message);
 		echo $pushStatus ."<br>";
 	}
-	
-	function put_latest_gcm_json($message){
-		file_put_contents("GcmJson.txt", json_encode($message));
-		echo "Writing json to text file done.<br>";
-	}
-	
+	*/
+	/*
 	//Generic php function to send GCM push notification
-   function sendMessageThroughGCM($registatoin_ids, $message) {
+	function sendMessageThroughGCM($registatoin_ids, $message) {
 		//Google cloud messaging GCM-API url
         $url = 'https://android.googleapis.com/gcm/send';
         $fields = array(
             'registration_ids' => $registatoin_ids,
+			'collapse_key' => 'my_collapsed_key',
             'data' => $message,
         );
 		
@@ -85,7 +180,8 @@
         curl_close($ch);
         return $result;
     }
-
+	*/
+	
 //see these urls for info	
 //http://deanhume.com/home/blogpost/push-notifications-on-the-web---google-chrome/10128
 //http://www.androidhive.info/2012/10/android-push-notifications-using-google-cloud-messaging-gcm-php-and-mysql/
